4c5e751f44c27d413eb4e16a2a2538f6f0c92a72
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-05-02 11:31:23 -0500

    updating work

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ create table if not exists albums
     id INT UNSIGNED NOT NULL AUTO_INCREMENT primary key ,
     artist VARCHAR(255) not null,
     name VARCHAR(255) not null,
-    release_date INT unsigned not null,
-    sales INT unsigned not null,
+    release_date date not null ,
+    sales float not null ,
     genre VARCHAR(255)not null
 );
